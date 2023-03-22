a17f6deef4a9afa13bbbb50f2c4c288ccdb3dfc8
Isaiah <isababa12@gmail.com>
2023-03-21 19:59:32 -0700

    linked list values complete

--- linkedListValues.js
+++ linkedListValues.js
@@ -1,10 +1,18 @@
-// class Node {
-//   constructor(val) {
-//     this.val = val;
-//     this.next = null;
-//   }
-// }
+class Node {
+    constructor(val) {
+      this.val = val;
+      this.next = null;
+    }
+  }
 
-const linkedListValues = (head) => {
-  
-};
+  const linkedListValues = (head) => {
+    const values = [];
+    _linkedListValues(head, values);
+    return values;
+  }
+
+  const _linkedListValues = (head, values) => {
+    if (head === null) return;
+    values.push(head.val);
+    _linkedListValues(head.next, values);
+  };
